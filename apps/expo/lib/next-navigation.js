57b531b441a4fd2a0407d53ac2a4baac60fed2f4
bizedadmin <bizedadmin@users.noreply.github.com>
2026-03-31 17:11:04 +0300

    chore: prepare for deployment

--- apps/expo/lib/next-navigation.js
+++
@@ -1,3 +0,0 @@
-export const useSearchParams = () => new URLSearchParams();
-export const useRouter = () => ({ push: () => {}, back: () => {}, replace: () => {} });
-export const usePathname = () => '';
